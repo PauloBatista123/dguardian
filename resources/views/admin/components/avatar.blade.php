d732a690aec540ccc5f01206c2a9ee309f7df263
PauloBatista123 <paulobatista.sistemas@gmail.com>
2023-08-30 14:34:47 -0300

    criação da dashboard e navbar

---
+++ resources/views/admin/components/avatar.blade.php
@@ -0,0 +1,19 @@
+@php
+$explodeName = explode(" ", auth()->user()->name);
+$firstName = $explodeName[0];
+$lastName = $explodeName[1];
+$iniciais = str_split($firstName)[0].str_split($lastName)[0];
+@endphp
+<div class="flex items-center space-x-4">
+  @if(auth()->user()->foto)
+  <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="">
+  @else
+  <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <span class="font-medium text-gray-600 dark:text-gray-300">{{$iniciais}}</span>
+  </div>
+  @endif
+  <div class="font-medium dark:text-white">
+    <div>{{auth()->user()->name}}</div>
+    <div class="text-sm text-gray-500 dark:text-gray-400">{{auth()->user()->email}}</div>
+  </div>
+</div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
       <div class="flex justify-center items-center flex-col gap-4 my-4 pb-3">
         <img class="object-cover" src="{{asset('novalogo.png')}}" alt="">
         <div class="my-3">
-          <span class="text-lg text-slate-600 dark:text-cyan-600 font-bold uppercase">{{ __('Autenticação no ambiente Sicoob Aracoop') }}</span>
+          <span class="text-lg text-slate-600 dark:text-turquesa font-bold uppercase">{{ __('Autenticação no ambiente Sicoob Aracoop') }}</span>
         </div>
       </div>
       @if(\Session::has('errors'))
@@ -19,7 +19,7 @@
         <form class="w-100 d-flex align-items-center flex-column" method="POST" action="{{ route('login') }}">
           @csrf
           <div class="w-50 my-2">
-            <label for="email" class="block mb-2 text-sm font-bold text-gray-900 dark:text-cyan-600">Seu email</label>
+            <label for="email" class="block mb-2 text-sm font-bold text-gray-900 dark:text-turquesa">Seu email</label>
             <input type="text" name="email" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500" placeholder="Email" required>
             @error('email')
             <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
@@ -27,7 +27,7 @@
           </div>
 
           <div class="w-50 my-2">
-            <label for="password" class="block mb-2 text-sm font-bold text-gray-900 dark:text-cyan-600">Sua senha</label>
+            <label for="password" class="block mb-2 text-sm font-bold text-gray-900 dark:text-turquesa">Sua senha</label>
             <input type="password" name="password" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg @error('password') is-invalid @enderror focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500" placeholder="Senha">
             @error('password')
             <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
@@ -35,9 +35,9 @@
           </div>
 
           <div class="w-50 mt-4">
-            <button type="submit" class="min-w-full relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <button type="submit" class="min-w-full relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-roxo to-blue-500 group-hover:from-roxo group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <span class="min-w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                {{ __('Login') }}
+                {{ __('Entrar') }}
               </span>
             </button>
           </div>
